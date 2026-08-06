af3ab6ce0beb6f48e60ba48b233cf0d06e32e4cf
Zack <me@zackaryf.com>
2026-08-06 17:54:24 -0400

    chore: make drizzle config self-contained

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,10 @@
-import { env } from '#/lib/env'
-import { config } from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
-
-config({ path: ['.env.local', '.env'] })
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/server/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL as string,
   },
 })
